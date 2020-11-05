09c73e9e26f06cd3dcfb1dee31642f1152f62a27
Jinyang Liu <47952494+lovejoyyoung@users.noreply.github.com>
2020-11-04 20:52:47 -0500

    Update User.java

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -3,6 +3,11 @@ package model;
 public class User {
     private int id;
     private String email;
+    private String firstname;
+    private String lastname;
+    private int age;
+    private int weight;
+    private int height;
     private String password;
 
 
@@ -11,9 +16,19 @@ public class User {
         this.password = password;
     }
 
-    public User(int id, String password) {
+    public User(int id, String email, String firstname, String lastname, int age, int weight, int height, String password) {
         this.id = id;
         this.password = password;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+
+    }
+
+    public User(String id, String email, String firstname, String lastname, int age, int weight, int height, String password) {
     }
 
     public int getId() {
@@ -26,6 +41,46 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public void setEmail(String email) {
